fa36eaadc9135905fa1ffbbfb1d4d2f1d64c6f14
Rostrv <trovatorosario91@gmail.com>
2022-04-21 14:35:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+
+        carousel: {
+            images: ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'],
+            titles: ['Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise', ],
+            texts: [
+                'Lorem ipsum, dolor sit amet.',
+                'Lorem ipsum.',
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit...',
+                'Et temporibus voluptatum suscipit tempore...',
+            ]
+
+
+        }
+
+
+    }
+})
