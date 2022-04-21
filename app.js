c29d622ad88854cf57a4e9f5cea9db8dda775d4f
Rostrv <trovatorosario91@gmail.com>
2022-04-21 17:33:28 +0200

    ended milestones

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 const app = new Vue({
     el: '#app',
-
     data: {
+        activeImage: 0,
+        carousel: [{
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            },
+        ],
+    },
 
-        carousel: {
-            images: ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'],
-            titles: ['Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise', ],
-            texts: [
-                'Lorem ipsum, dolor sit amet.',
-                'Lorem ipsum.',
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit...',
-                'Et temporibus voluptatum suscipit tempore...',
-            ]
+    methods: {
 
+        prevImage() {
+            if (this.activeImage === 0)
+                this.activeImage = this.carousel.length
+            console.log('prev image')
+            this.activeImage--
+        },
 
+        nextImage() {
+
+            console.log('next image')
+            this.activeImage++
+                if (this.activeImage === this.carousel.length) {
+                    this.activeImage = 0
+                }
         }
 
 
     }
-})
+});
